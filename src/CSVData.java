7fbea4554b1d1f53f7b1cb70444f44531fb3b328
vle772 <vle772@fhs.win.fuhsd.org>
2016-10-12 11:10:16 -0700

    Added more methos stubs to CSVData.java

--- src/CSVData.java
+++ src/CSVData.java
@@ -22,7 +22,56 @@ public class CSVData {
 	public static CSVData readCSVFile(String filename, int numLinesToIgnore){
 		return null;
 	}
+	
 
+	public double[] getRow(int row){
+		return null;
+	}
+	
+	public double[] getColumn(String name){
+		return null;
+	}
+	
+	public double[][] getRows(int startRow, int endRow){
+		return null;
+	}
+	
+	public double[][] getRows(int[] rowIndexes){
+		return null;
+	}
+	
+	public double[][] getColumns(int startCol, int endCol){
+		return null;
+	}
+	
+	public double[][] getColumns(int[] colIndexes){
+		return null;
+	}
+	
+	public double[][] getColumns(String[] colNames){
+		return null;
+	}
+	
+	public void setValue(int rowIndex, int colIndex, double newValue){
+		
+	}
+	
+	public void setRow(int row){
+		
+	}
+	
+	public void setCol(int col){
+		
+	}
+	
+	public String[] getColumnNames(){
+		return null;
+	}
+	
+	
+	public void saveToFile(String filename){
+		
+	}
 	
 	
 
